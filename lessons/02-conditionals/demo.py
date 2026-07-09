713cff10ee1188397447f480c9e8eaecd4063dbc
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2026-07-09 15:30:09 -0400

    Update workflow files:

--- lessons/02-conditionals/demo.py
+++
@@ -1,9 +0,0 @@
-# LIVE DEMO (Teacher projects this)
-wallet = 50
-item_price = 50
-
-# 🔥 THE TRAP: Strict inequality check fails when values match perfectly!
-if wallet > item_price:
-    print("Purchase Approved!")
-else:
-    print("Error: Insignificant Funds!")
